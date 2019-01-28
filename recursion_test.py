64a1e672a8fbc4e1542a49abb01e55c484e92908
Egon Fiedler <efiedler93@gmail.com>
2019-01-28 11:02:49 -0800

    Added more test cases but did not made it pass the case where it find a string

--- recursion_test.py
+++ recursion_test.py
@@ -18,6 +18,10 @@ class RecursionTest(unittest.TestCase):
         assert fibonacci(8) == 21
         assert fibonacci(9) == 34
         assert fibonacci(10) == 55
+    
+    def test_fibonacci_with_medium_integers(self):
+        assert fibonacci(11) == 89
+        assert fibonacci(12) == 144
 
     def test_fibonacci_with_large_integers(self):
         # These could run for a long time, depending on your implementation...
@@ -26,9 +30,9 @@ class RecursionTest(unittest.TestCase):
         assert fibonacci(25) == 75025
         # TODO: Improve the runtime of your fibonacci function for these cases
         # If you're not careful, you may need to be very patient for these...
-        # assert fibonacci(30) == 832040
-        # assert fibonacci(35) == 9227465
-        # assert fibonacci(40) == 102334155
+        assert fibonacci(30) == 832040
+        assert fibonacci(35) == 9227465
+        assert fibonacci(40) == 102334155
 
     def test_fibonacci_with_negative_integers(self):
         # Should raise a ValueError for n < 0
@@ -43,6 +47,8 @@ class RecursionTest(unittest.TestCase):
             fibonacci(2.0)
         with self.assertRaises(ValueError):
             fibonacci(3.14159)
+
+    def test_fibonacci_with_strings(self):
         with self.assertRaises(ValueError):
             fibonacci('5')
 
