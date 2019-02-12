5c3f55b0e84264b47f529ed5f6c3ddebe4ae6405
Egon Fiedler <efiedler93@gmail.com>
2019-02-12 02:45:23 -0800

    Getting up to date with the class

--- Code/try_a_trie.py
+++ Code/try_a_trie.py
@@ -7,7 +7,8 @@
 '''' To answer this question let's see for what it was made ''''
 
 ##### For what it was made
-''' It's role is that of organizing letters in the alphabet '''
+''' It's role is that of organizing letters in the alphabet.
+    You use it to complete words, autocorrector is a very good one '''
 
 ##### How it works
 ''' It's a series of linked list that all connect to an empty node '''
@@ -17,13 +18,15 @@
   *  It is has long as there are values variations
   *  Tends to begin on an empty string
   *  Both the value and the value reference could be empty
-  *  Each node contains an array of references/links, it is defined by the links index
-  *  Nodes can contain two things a value which might be null and references to a child node that might also be null
-  *  It has an index and it's lenght depends on the possible number of variations of what's getting on it
+  *  Each node contains an array of references/links, it is defined 
 '''
+class Node(object):
+  
 
-##### Confusion
-'''
-Is it that because of it's nature it relies on having has max lenght the total number of possible variations, but then wouldn't it 
-become a humongous amount of possible nodes, also it seems that the index is the element that makes this be able to happen
-'''
+class Trie(object):
+  #Create a list with the required amount of spaces for this particular problem
+  #With the context of the Abecedario been 26 letters, the list will have 26 letters
+
+  complete_list = list()
+
+
---
+++ Code/try_a_trie_test.py
@@ -0,0 +1,10 @@
+'''
+It's neccesary to always start with tests to make your algorithm robust. 
+'''
+
+'''
+WHAT THE FUCK IS A TRIE, I"M NOT SMART ENOUGH TO WRITE THAT SHIT, IT'S TIME TO FIGURE OUT HOW THE FUCK THIS WILL BE DONE
+'''
+
+import pytest
+
