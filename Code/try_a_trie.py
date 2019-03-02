04a3dc8417c8ac475b4348f3286af09870784f0d
Egon Fiedler <efiedler93@gmail.com>
2019-03-02 15:25:35 -0800

    improved my approach towards getting the index

--- Code/try_a_trie.py
+++ Code/try_a_trie.py
@@ -10,11 +10,11 @@
 ##### For what it was made
 ''' It's role is that of organizing letters in the alphabet.
     You use it to complete words, autocorrector is a very good use 
-    example of a trie in action
+    example of a trie in action.
 '''
 
 ##### How it works
-''' It's a series of linked list that all connect to an empty node 
+''' On this scenario, every node an array of 26 spaces and a property end of word.
 '''
 
 ##### Traits
@@ -24,47 +24,39 @@
     * Each node contains an array of references/links, it is defined 
 '''
 
-##### Big (O) notation
-
 #Every node represents one letter of some prefix
 class Node(object):
   def __init__(self):
     self.children = [None]*26
-    # self.data = ""
-    # Because maybe
     self.end_of_word = False
-    # self.is_capital_letter = False
 
 class Trie(object):
 
   def __init__(self):
-    self.root = self.get_node()
-    # root = self.get_node()
-    self.index_of_letter = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7, 
-                            "i":8, "j":9, "k":10, "l":11, "m":12, "n":13, "o":14, 
-                            "p":15, "q":16, "r":17, "s":18, "t":19, "u":20, "v":21, 
-                            "w":22, "x":23, "y":24, "z":25}
+    self.root = Node()
 
-  def get_node():
-    return Node()
+  def get_index(self, letter):
+    return ord(letter) - 97
 
-  def add_word(self, word):
+  def add_letter_by_letter_the_word(self, word):
     # BIG(O)Notation:
     # Expected Time Complexity: O(k) => Where 'k' is the average lenght of a string
     # Expected Space Complexity: O(number of possible values??)
     # Current Time Complexity: ???
     # Current Space Complexity: ???
     # Using root from initializer
+
     node = self.root
     
     for letter in word:
-      index = self.maps_element_to_alfabet_index(letter)
-      # Possible values
-      # Coudl ahve a node
+      # This line should map the corresponding letter to it's index
+      index = self.get_index(letter)
+      # if on the node children (the list or python dynamic array with 26 spaces),
+      # that space is empty
       if node.children[index] is None:
         # Create a new node for that index
-        new_node = self.get_node()
-        # 
+        new_node = Node()
+        
         node.children[index] = new_node
 
         node = new_node
@@ -74,7 +66,7 @@ class Trie(object):
     node.end_of_word = True
 
   def contains(self, word):
-    """Return True if the trie constains the given word, otherwise False."""
+    """Return True if the trie contains the given word, otherwise False."""
     # BIG(O)Notation:
     # Expected Time Complexity: O(log(N))
     # Expected Space Complexity: O(log(N))
@@ -90,29 +82,27 @@ class Trie(object):
     #  * Every character is stored in a different level of the word
     
     # Approach:
-    #  * You have find the looked after word
+    #  * You have to find the looked after word
     #  * There will only be as many children has the longest word
     #  * If the word exist at the end of it, the complete word must be there and
     #    the node property end of word would be true
 
     # THINK OF ADDING something 
-
     # Mark the place where to begin at
-    starting_point = self.root
+    node = self.root
     # The lenght of the word is has many levels ahs you have to search for
-    number_of_levels = len(word)
-    # Iterate through has many level has long the word is
-    for each_level in range(number_of_levels):
-      # the index is the 
+    for letter in word:
+      index = self.get_index(letter)
 
-      index = self.children[i]
-      index_of_letter(word[])
-      if starting_point.children[index] is None:
+      if node.children[index] is None:
         return False
-      if each_level[index].end_of_word == True:
-        return True
 
-  def insert():
+    if node.end_of_word == True:
+      return True
+    
+    return False
+
+  def insert(self):
     pass
   
 def main():
